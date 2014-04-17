8dd912fa1e7a378ef73468b04e473f81a8061fbb
岩井 龍之介 <ryunosuke.iwai@gmail.com>
2014-04-18 01:15:29 +0900

    implemented GameOverMenu

--- main.py
+++ main.py
@@ -30,10 +30,10 @@ Clock = pygame.time.Clock()
 Scale = 2
 EPS = 5
 unit = 20
-back = pygame.transform.scale(pygame.image.load("data/back.jpg"),Size)
-wall = pygame.transform.scale(pygame.image.load("data/wall.jpg"),(unit,unit))
-plane = pygame.transform.scale(pygame.image.load("data/plane.png"),(unit,unit))
-menu = pygame.transform.scale(pygame.image.load("data/gameover.jpg"),Size)
+back = pygame.transform.scale(pygame.image.load("data/back.jpg").convert(),Size)
+wall = pygame.transform.scale(pygame.image.load("data/wall.jpg").convert(),(unit,unit))
+plane = pygame.transform.scale(pygame.image.load("data/plane.png").convert_alpha(),(unit,unit))
+menu = pygame.transform.scale(pygame.image.load("data/gameover.png").convert_alpha(),Size)
 RED = (255,0,0)
 GREEN = (39,203,81)
 BLUE = (123,48,255)
@@ -60,11 +60,14 @@ class Game():
 		self.dbstr2 = ""
 		self.start = 0
 		self.font = pygame.font.Font(None,35)
+		self.midfont = pygame.font.Font(None,60)
+		self.bigfont = pygame.font.Font(None,100)
 		self.pa = pyaudio.PyAudio()
 		self.data = []
 		self.recording = pyaudio.paContinue
 		self.scene = 1
 		self.cnt = 0
+		self.select = 0
 		self.freqList = []
 #音声解析部分
 	def limit(self,x,a,b):
@@ -117,11 +120,8 @@ class Game():
 	def debug(self,string):
 		self.dbstr = string
 	def gameover(self):
-		print "here"
 		if self.cnt <= 60:
 			self.cnt+=1
-		else:
-			self.__init__()
 	def update(self):
 		if self.scene == 1:
 			if not self.check():
@@ -156,6 +156,13 @@ class Game():
 		for event in pygame.event.get():
 			if event.type == QUIT:
 				exit()	
+			if self.scene == 2 and self.cnt > 60:
+				if event.type == KEYDOWN and event.key == K_RIGHT:
+					self.select = (self.select + 1) % 2
+				if event.type == KEYDOWN and event.key == K_LEFT:
+					self.select = (self.select + 1) % 2
+				if event.type == KEYDOWN and event.key == K_RETURN:
+					self.__init__()
 	def make_wall(self):
 		if self.x == self.nxt:
 #			if random.randint(0,4)<=3:
@@ -173,8 +180,10 @@ class Game():
 			if self.todraw[p][1] - unit + EPS < self.y and self.y < self.todraw[p][2] - EPS:
 				return False
 		return True
+	def pos(self,size):
+		return (X - size[0]) / 2
 	def draw(self):
-		if self.scene == 1:
+		if self.scene == 1 or self.scene == 2:
 			Screen.blit(back,(0,0))
 			Screen.blit(plane,(PX,self.y))
 			Screen.blit(self.font.render(self.dbstr,True,BLACK),(0,0))
@@ -186,8 +195,16 @@ class Game():
 				b = d[2]
 				for i in range(a,b,unit):
 					Screen.blit(wall,(x-self.x,i))
-		if self.scene == 2 and self.cnt <= 60:
-			Screen.blit(menu,(0,-Y+Y/60*self.cnt))
+		if self.scene == 2:
+			if self.cnt <= 60:
+				Screen.blit(menu,(0,-Y+Y/60*self.cnt))
+			else:
+				Screen.blit(menu,(0,0))
+				Screen.blit(self.bigfont.render(str(self.x)+"m",True,WHITE),(self.pos(self.bigfont.size(str(self.x)+"m")),100))
+				Screen.blit(self.midfont.render("Continue?",True,WHITE),(self.pos(self.midfont.size("Continue?")),300))
+				Screen.blit(self.midfont.render("*",True,WHITE),(150+self.select*300,400))
+				Screen.blit(self.midfont.render("YES",True,WHITE),(200,400))
+				Screen.blit(self.midfont.render("NO",True,WHITE),(500,400))
 		pygame.display.update()
 
 def main():
